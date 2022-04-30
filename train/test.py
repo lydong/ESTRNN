dc27cd2c2223bc60b46be043510c62d3573cd839
lydong <89819980+lydong@users.noreply.github.com>
2022-04-29 21:48:46 -0400

    Add files via upload

--- train/test.py
+++ train/test.py
@@ -38,6 +38,9 @@ def test(para, logger):
     elif ds_name == 'gopro_ds_lmdb' or ds_name == 'reds_lmdb':
         ds_type = 'valid'
         _test_lmdb(para, logger, model, ds_type)
+    elif ds_name == 'REDS_png':
+        ds_type = 'valid'
+        _test_torch_SR(para, logger, model, ds_type)
     else:
         raise NotImplementedError
 
@@ -49,7 +52,9 @@ def _test_torch(para, logger, model, ds_type):
     results_register = set()
     H, W = 480, 640
     val_range = 2.0 ** 8 - 1 if para.data_format == 'RGB' else 2.0 ** 16 - 1
-    dataset_path = join(para.data_root, para.dataset, '{}_{}'.format(para.dataset, para.ds_config), ds_type)
+    #dataset_path = join(para.data_root, para.dataset, '{}_{}'.format(para.dataset, para.ds_config), ds_type)
+    #print(dataset_path)
+    dataset_path = 'Dataset/2ms16ms'
     seqs = sorted(os.listdir(dataset_path))
     # seq_length = 100
     seq_length = 150
@@ -139,6 +144,100 @@ def _test_torch(para, logger, model, ds_type):
     logger('Average time per image: {}'.format(timer.avg))
 
 
+def _test_torch_SR(para, logger, model, ds_type):
+    PSNR = AverageMeter()
+    SSIM = AverageMeter()
+    timer = AverageMeter()
+    results_register = set()
+    H, W = 180, 420
+    val_range = 2.0 ** 8 - 1 if para.data_format == 'RGB' else 2.0 ** 16 - 1
+    #===== Here to change paths =====
+    dataset_path = '/scratch/gilbreth/dong33/REDS_png/valid'
+    seq_length = 100
+    save_dir = '/scratch/gilbreth/dong33/REDS_png/valid/model_M2/guess_model_epoch200'
+    os.makedirs(save_dir, exist_ok=True)
+    #========================================================
+    seqs = sorted(os.listdir(join(dataset_path, 'valid_sharp')))
+    for seq in seqs:
+        logger('seq {} image results generating ...'.format(seq))
+        suffix = 'png' if para.data_format == 'RGB' else 'tiff'
+        start = 0
+        end = para.test_frames
+        while True:
+            input_seq = []
+            label_seq = []
+            for frame_idx in range(start, end):
+                blur_img_path = join(dataset_path, 'valid_sharp_bicubic/X4', seq, '{:08d}.{}'.format(frame_idx, suffix))
+                sharp_img_path = join(dataset_path, 'valid_sharp', seq, '{:08d}.{}'.format(frame_idx, suffix))
+                if para.data_format == 'RGB':
+                    blur_img = cv2.imread(blur_img_path).transpose(2, 0, 1)[np.newaxis, ...]
+                    gt_img = cv2.imread(sharp_img_path)
+                else:
+                    blur_img = cv2.imread(blur_img_path, -1)[..., np.newaxis].astype(np.int32)
+                    blur_img = blur_img.transpose(2, 0, 1)[np.newaxis, ...]
+                    gt_img = cv2.imread(sharp_img_path, -1).astype(np.uint16)
+                input_seq.append(blur_img)
+                label_seq.append(gt_img)
+            input_seq = np.concatenate(input_seq)[np.newaxis, :]
+            model.eval()
+            with torch.no_grad():
+                input_seq = normalize(torch.from_numpy(input_seq).float().cuda(), centralize=para.centralize,
+                                      normalize=para.normalize, val_range=val_range)
+                time_start = time.time()
+                output_seq = model([input_seq, ])
+                if isinstance(output_seq, (list, tuple)):
+                    output_seq = output_seq[0]
+                output_seq = output_seq.squeeze(dim=0)
+                timer.update((time.time() - time_start) / len(output_seq), n=len(output_seq))
+            for frame_idx in range(para.past_frames, end - start - para.future_frames):
+                blur_img = input_seq.squeeze(dim=0)[frame_idx]
+                blur_img = normalize_reverse(blur_img, centralize=para.centralize, normalize=para.normalize,
+                                             val_range=val_range)
+                blur_img = blur_img.detach().cpu().numpy().transpose((1, 2, 0)).squeeze()
+                blur_img = blur_img.astype(np.uint8) if para.data_format == 'RGB' else blur_img.astype(np.uint16)
+                blur_img_path = join(save_dir, '{:08d}_input.{}'.format(frame_idx + start, suffix))
+                gt_img = label_seq[frame_idx]
+                #gt_img_path = join(save_dir, '{:08d}_gt.{}'.format(frame_idx + start, suffix))
+                deblur_img = output_seq[frame_idx - para.past_frames]
+                deblur_img = normalize_reverse(deblur_img, centralize=para.centralize, normalize=para.normalize,
+                                               val_range=val_range)
+                deblur_img = deblur_img.detach().cpu().numpy().transpose((1, 2, 0)).squeeze()
+                deblur_img = np.clip(deblur_img, 0, val_range)
+                deblur_img = deblur_img.astype(np.uint8) if para.data_format == 'RGB' else deblur_img.astype(np.uint16)
+                deblur_img_path = join(save_dir, seq, '{:08d}_{}.{}'.format(frame_idx + start, para.model.lower(), suffix))
+                cv2.imwrite(deblur_img_path, deblur_img)
+                if deblur_img_path not in results_register:
+                    results_register.add(deblur_img_path)
+                    PSNR.update(psnr_calculate(deblur_img, gt_img, val_range=val_range))
+                    SSIM.update(ssim_calculate(deblur_img, gt_img, val_range=val_range))
+
+            if end == seq_length:
+                break
+            else:
+                start = end - para.future_frames - para.past_frames
+                end = start + para.test_frames
+                if end > seq_length:
+                    end = seq_length
+                    start = end - para.test_frames
+
+        if para.video:
+            if para.data_format != 'RGB':
+                continue
+            logger('seq {} video result generating ...'.format(seq))
+            marks = ['Input', para.model, 'GT']
+            path = dirname(save_dir)
+            frame_start = para.past_frames
+            frame_end = seq_length - para.future_frames
+            img2video(path=path, size=(3 * W, 1 * H), seq=seq, frame_start=frame_start, frame_end=frame_end,
+                      marks=marks, fps=10)
+
+
+    logger('Test images : {}'.format(PSNR.count), prefix='\n')
+    logger('Test PSNR : {}'.format(PSNR.avg))
+    logger('Test SSIM : {}'.format(SSIM.avg))
+    logger('Average time per image: {}'.format(timer.avg))
+
+
 def _test_lmdb(para, logger, model, ds_type):
     PSNR = AverageMeter()
     SSIM = AverageMeter()
@@ -146,7 +245,7 @@ def _test_lmdb(para, logger, model, ds_type):
     results_register = set()
     if para.dataset == 'gopro_ds_lmdb':
         B, H, W, C = 1, 540, 960, 3
-    elif para.dataset == 'reds_lmdb':
+    elif para.dataset == 'reds_ds_lmdb':
         B, H, W, C = 1, 720, 1280, 3
     data_test_path = join(para.data_root, para.dataset, para.dataset[:-4] + ds_type)
     data_test_gt_path = join(para.data_root, para.dataset, para.dataset[:-4] + ds_type + '_gt')
